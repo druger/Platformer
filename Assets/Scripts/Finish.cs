1d0612efcf38d262c9d95b54ac68cd4678fdaf33
Evgeny Gromov <bayt32@yandex.ru>
2021-08-09 14:37:56 +0300

    Add suggestions

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Finish : MonoBehaviour {
+    [SerializeField] private GameObject messageUI;
+    
     private bool _isActivated;
 
     public void Activate() {
         _isActivated = true;
+        messageUI.SetActive(false);
     }
     
     public void FinishLevel() {
         if (_isActivated) {
             gameObject.SetActive(false);
+        } else {
+            messageUI.SetActive(true);
         }
     }
 }
